a442e4f062f46fc362729a5f6c7ef4bd8e4fd983
Koaru <mucsanyi.b@gmail.com>
2022-08-22 11:41:59 +0200

    add black and white pawns to the board

--- src/main/java/com/codecool/polishdraughts/Board.java
+++ src/main/java/com/codecool/polishdraughts/Board.java
@@ -5,9 +5,16 @@ public class Board {
 
     public Board(int n) {
         Pawn = new int[n][n];
+        int whitePawns = 2 * n;
+        int blackPawns = 2 * n;
         for (int x = 0; x < n; x++) {
             for (int y = 0; y < n; y++) {
-                Pawn[x][y] = 0;
+                if ((x + y) % 2 == 1 && blackPawns > 0){
+                    Pawn[x][y] = 1;
+                    Pawn[n - x - 1][n - y - 1] = 2;
+                    blackPawns--;
+                    whitePawns--;
+                }
             }
         }
     }
