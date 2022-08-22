1051b55d137f296ddfaf869616b733b3021f2d85
rebekajakob <rebusacka@gmail.com>
2022-08-22 14:46:21 +0200

    create coordinatesPosition class and make an instance in Pawn constructor

--- src/main/java/com/codecool/polishdraughts/Pawn.java
+++ src/main/java/com/codecool/polishdraughts/Pawn.java
@@ -2,8 +2,11 @@ package com.codecool.polishdraughts;
 
 public class Pawn{
     String color;
-    public Pawn(String color) {
+    coordinatesPosition coordinates;
+    public Pawn(String color, Integer x, Integer y) {
         this.color = color;
+        this.coordinates = new coordinatesPosition(x,y);
+
     }
 
     public String getColor() {
---
+++ src/main/java/com/codecool/polishdraughts/coordinatesPosition.java
@@ -0,0 +1,11 @@
+package com.codecool.polishdraughts;
+
+public class coordinatesPosition {
+    Integer x;
+    Integer y;
+
+    public coordinatesPosition(Integer x, Integer y) {
+        this.y = y;
+        this.x = x;
+    }
+}
