af979b80f761aca413a8dddbbc54cd08430980cc
rebekajakob <rebusacka@gmail.com>
2022-08-23 14:54:48 +0200

    create playRound() method

--- src/main/java/com/codecool/polishdraughts/Game.java
+++ src/main/java/com/codecool/polishdraughts/Game.java
@@ -3,6 +3,8 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Game {
+    String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int boardLength;
     public int getValidBoardSize(){
         Scanner myVar = new Scanner(System.in);
         int inputBoardSize;
@@ -14,6 +16,7 @@ public class Game {
             }
             System.out.println("Error! Board size must be between 10 and 20!");
         }
+        boardLength = inputBoardSize;
         return inputBoardSize;
     }
     public String checkForWinner(Pawn[][] fields){
@@ -53,7 +56,7 @@ public class Game {
         return "None";
     }
 
-    public boolean userInputValidation(String userInput, String abc, int boardLength){
+    public boolean userInputValidation(String userInput){
         if (userInput.matches("^[a-zA-Z]\\d+$")){
             if (abc.indexOf(userInput.charAt(0)) < boardLength){
                 if (Integer.parseInt(userInput.substring(1)) <= boardLength){
@@ -62,5 +65,32 @@ public class Game {
             }
         }
         return false;
+    }
+
+    public void playRound(String currentPlayer,Pawn[][] fields){
+        int row;
+        int col;
+        Scanner myVar = new Scanner(System.in);
+        while (true){
+            System.out.println("Please choose a draught to move with: ");
+            String userInput = myVar.nextLine().toUpperCase();
+            if (userInputValidation(userInput)){
+                row = abc.indexOf(userInput.charAt(0));
+                col = Integer.parseInt(userInput.substring(1)) -1;
+                if (Objects.equals(fields[row][col].color, currentPlayer) && fields[row][col].canPawnMoveAnywhere(fields)) {
+                    break;
+                }
+            }
+            System.out.println("Please choose a draught that's yours and can move!");
+        }
+
+
+
+        if (currentPlayer == "white"){
+            currentPlayer = "black";
+        } else {
+            currentPlayer = "white";
+        }
+
     }
 }
