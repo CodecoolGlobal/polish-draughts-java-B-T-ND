2fbd9adfe2c5ee168d4ad528e448c6b570be6e4c
Botond <63496999+B-T-ND@users.noreply.github.com>
2022-08-23 10:27:32 +0200

    add toString() method to board to return printable board view

--- src/main/java/com/codecool/polishdraughts/polishdraughts.java
+++ src/main/java/com/codecool/polishdraughts/polishdraughts.java
@@ -1,12 +1,11 @@
 package com.codecool.polishdraughts;
 
-import java.util.Arrays;
 
 public class polishdraughts {
     public static void main(String[] args) {
         Game game = new Game();
 //        game.getValidBoardSize();
-        Board board = new Board(15);
-        System.out.println(Arrays.deepToString(board.fields));
+        Board board = new Board(10);
+        System.out.println(board);
     }
 }
